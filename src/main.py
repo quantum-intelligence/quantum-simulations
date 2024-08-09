871a755fb930c255f72483ba129013df4665bc9c
Trevor Rhone <tdrhone@gmail.com>
2024-08-09 10:35:36 -0400

    adding main.py and myfunctions.py

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# This is the main python code that calls other functions
+
+import numpy as np
+import pandas as pd
+
+from myfunctions import *
+
+a = 2
+b = 3
+
+print(a,b) 
+
+print(myadd(a,b))
+
+print(mymultiply(a,b))
---
+++ src/myfunctions.py
@@ -0,0 +1,10 @@
+#helper functions
+
+def myadd(a,b):
+    return a+b
+
+#this needs to be modified
+def mymultiply(a,b):
+    return 0
+
+#feel free to add more functions
